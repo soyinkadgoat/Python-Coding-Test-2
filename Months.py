53cb0fcbc72d936394523c733ef279c4d7041c48
soyinkadgoat <soyinkadgreat807@gmail.com>
2026-07-26 11:23:00 +0100

    Add files via upload

---
+++ Months.py
@@ -0,0 +1,6 @@
+Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+print("The months in a year are:")
+
+for i in Months:
+    print(i)
